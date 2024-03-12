23b9742caf505fd9763fd7b0a359fb4dfc4a3c09
einarsskutans <einarsskutans@gmail.com>
2024-03-12 16:42:37 +0200

    color

--- main.cpp
+++ main.cpp
@@ -19,33 +19,46 @@ struct Center {
     double x;
     double y;
 };
+struct Color {
+    Color(unsigned char pR = 255, unsigned char pG = 255, unsigned char pB = 255) { // Default color = white
+        r = pR;
+        g = pG;
+        b = pB;
+    };
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+};
+
 class Circle : public Figure {
     public:
         Center center;
-        Circle(double pr, double pvelX, double pvelY) {
+        Color color;
+        Circle(double pRadius, double pVelX, double pVelY, Color pColor) {
             Reset();
-            r = pr;
-            velX = pvelX;
-            velY = pvelY;
+            radius = pRadius;
+            velX = pVelX;
+            velY = pVelY;
+            color = pColor;
         }
         void Reset() {
-            center.x = SCREEN_W / 2;
-            center.y = SCREEN_H / 2;
+            center.x = SCREEN_W/2;
+            center.y = SCREEN_H/2;
         }
         void Draw() {
-            al_draw_filled_circle(center.x, center.y, r, al_map_rgb( 0, 255, 0 ) );
+            al_draw_filled_circle(center.x, center.y, radius, al_map_rgb(color.r, color.g, color.b) );
         }
         void Move() {
-            if (center.x > SCREEN_W-r) {
+            if (center.x > SCREEN_W-radius) {
                 velX = -velX;
             }
-            if (center.y > SCREEN_H-r) {
+            if (center.y > SCREEN_H-radius) {
                 velY = -velY;
             }
-            if (center.x < 0+r) {
+            if (center.x < 0+radius) {
                 velX = -velX;
             }
-            if (center.y < 0+r) {
+            if (center.y < 0+radius) {
                 velY = -velY;
             }
             center.x = center.x + velX;
@@ -55,7 +68,7 @@ class Circle : public Figure {
     private:
         double velX;
         double velY;
-        double r; // radius
+        double radius;
 };
 class Square : public Figure {
     public:
@@ -68,19 +81,19 @@ class Square : public Figure {
             velY = pvelY;
         }
         void Draw() {
-            al_draw_filled_rectangle(center.x-(width/2),center.y-(height/2), center.x+(width/2), center.y+(height/2), al_map_rgb( 0, 255, 0 ));
+            al_draw_filled_rectangle(center.x - (width/2), center.y - (height/2), center.x + (width/2), center.y + (height/2), al_map_rgb( 0, 255, 0 ));
         }
         void Move() {
-            if (center.x > SCREEN_W-width/2) {
+            if (center.x > SCREEN_W - width/2) {
                 velX = -velX;
             }
-            if (center.y > SCREEN_H-height/2) {
+            if (center.y > SCREEN_H - height/2) {
                 velY = -velY;
             }
-            if (center.x < 0+width/2) {
+            if (center.x < 0 + width/2) {
                 velX = -velX;
             }
-            if (center.y < 0+height/2) {
+            if (center.y < 0 + height/2) {
                 velY = -velY;
             }
             center.x = center.x + velX;
@@ -92,8 +105,8 @@ class Square : public Figure {
         double width;
         double height;
         void Reset() {
-            center.x = SCREEN_W/2;
-            center.y = SCREEN_H/2;
+            center.x = SCREEN_W / 2;
+            center.y = SCREEN_H / 2;
         }
 };
 class Screensaver {
@@ -114,15 +127,16 @@ class Screensaver {
             }
         }
     private:
-        Square square1{50,50,5,5};
-        Square square2{50,50,10,5};
-        Square square3{50,50,10,20};
-        Circle circle1{25,5,10};
-        Circle circle2{25,25,10};
-        Circle circle3{25,10,50};
+        Color newColor{0, 0, 255};
+        Square square1{50, 50, 5, 5};
+        Square square2{50, 50, 10, 5};
+        Square square3{50, 50, 10, 20};
+        Circle circle1{25, 5, 10, newColor};
+        Circle circle2{25, 25, 10, newColor};
+        Circle circle3{25, 10, 50, newColor};
 
         typedef Figure* PFigure;    
-        PFigure PFigures[6] = {&square1,&square2,&square3, &circle1,&circle2,&circle3};
+        PFigure PFigures[6] = {&square1, &square2, &square3, &circle1, &circle2, &circle3};
 
 };
 Screensaver screensaver;
