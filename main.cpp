b553588795b4ef7ad7811702813535ceee6c366f
einarsskutans <einarsskutans@gmail.com>
2024-03-18 21:16:53 +0200

    some patches

--- main.cpp
+++ main.cpp
@@ -45,7 +45,6 @@ class Figure {
         virtual void Move() {
             centerVel = Physics::CollideBounds(center.x, center.y, velX, velY, width, height);
 
-
             velX = centerVel.x, velY = centerVel.y; // Unpack centerVel into 2 vel axis
             center.x += centerVel.x;
             center.y += centerVel.y;
